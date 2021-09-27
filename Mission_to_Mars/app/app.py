1b0e048ca20306d2be9ae781e9bdff874e1b75c2
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-09-27 13:30:21 -0700

    build initial tree, visit NASA site, save article

---
+++ Mission_to_Mars/app/app.py
@@ -0,0 +1,16 @@
+# import Flask, pymongo, and scrape_mars (your python file)
+
+# Instantiate a Flask app
+
+
+# Use flask_pymongo to set up mongo connection
+
+# Create a base '/' route that will query your mongodb database and render the `index.html` template
+
+
+# Create a '/scrape' route that will create the mars collection, run your scrape() function from scrape_mars, and update the mars collection in the database
+# The route should redirect back to the base route '/' with a code 302.
+
+
+
+# Run your app
---
+++ Mission_to_Mars/app/scrape_mars.py
@@ -0,0 +1,5 @@
+# Define a function called `scrape` that will execute all of your scraping code from the `mission_to_mars.ipynb` notebook and return one Python dictionary containing all of the scraped data. 
+
+# It will be a good idea to create multiple smaller functions that are called by the `scrape()` function. 
+# Remember, each function should have one 'job' (eg. you might have a `mars_news()` function that scrapes the NASA mars news site and returns the content as a list/tuple/dictionary/json)
+# HINT: the headers in the notebook can serve as a useful guide to where one 'job' ends and another begins. 
