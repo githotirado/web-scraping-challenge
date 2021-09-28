fc149e505fb6c4e968e747dcb0adf6da4a71ea1a
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-09-28 09:13:45 -0700

    app.py and flask_mongo

--- Mission_to_Mars/app/app.py
+++ Mission_to_Mars/app/app.py
@@ -14,18 +14,22 @@ mongo = PyMongo(app)
 # Create a base '/' route that will query your mongodb database and render the `index.html` template
 @app.route("/")
 def mainpage():
+    # Get the Mars data from MongoDB
+    mars_dictionary = mongo.db.collection.find_one()
 
-# Create a '/scrape' route that will create the mars collection, run your scrape() function from scrape_mars, and update the mars collection in the database
-# The route should redirect back to the base route '/' with a code 302.
+    # Use Mars data in render template for index.html
+    return render_template("index.html", mars_dictionary=mars_dictionary)
+
+# Route that will create the mars collection
+# The route will redirect back to the base route '/' with a code 302.
 @app.route("/scrape")
 def call_scrape():
     # Call scrape_mars.scrape and save dictionary
     mars_dict = scrape()
     # Store response as a dictionary in MongoDB
-    # mongo.db.collection.update({}, mars_dict, upsert=True)
-    mongo.db.collection.insert_one(mars_dict)
+    mongo.db.collection.update({}, mars_dict, upsert=True)
+    # mongo.db.collection.insert_one(mars_dict)
     return redirect("/", 302)
-
 
 # Run the app
 if __name__ == "__main__":
