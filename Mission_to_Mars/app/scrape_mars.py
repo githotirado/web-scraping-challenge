29c491096b70e92bddb0a3bf21e6294e56eec9a9
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-09-27 21:28:42 -0700

    Mars scraper python initialize

--- Mission_to_Mars/app/scrape_mars.py
+++ Mission_to_Mars/app/scrape_mars.py
@@ -2,4 +2,26 @@
 
 # It will be a good idea to create multiple smaller functions that are called by the `scrape()` function. 
 # Remember, each function should have one 'job' (eg. you might have a `mars_news()` function that scrapes the NASA mars news site and returns the content as a list/tuple/dictionary/json)
-# HINT: the headers in the notebook can serve as a useful guide to where one 'job' ends and another begins. 
+# HINT: the headers in the notebook can serve as a useful guide to where one 'job' ends and another begins.
+
+# Import Splinter, BeautifulSoup, and Pandas
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import time
+
+def scrape(web_url):
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    browser.visit(web_url)
+    time.sleep(5)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    # browser.quit()
+    return 
+
+def visit_nasa_mars_news():
+    # Visit the mars nasa news site
+    mnns_url = "https://mars.nasa.gov/news/"
+    scrape(mnns_url)
